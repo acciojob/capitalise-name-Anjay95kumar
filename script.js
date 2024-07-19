2f831e899e412102f395a724165928db3bc03295
Anjay95kumar <130925026+Anjay95kumar@users.noreply.github.com>
2024-07-19 19:23:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+//your JS code here. If required.
+
+const input = document.getElementById('fname');
+
+input.addEventListener("blur", function(){
+	let name = input.value;
+	name = name.toUpperCase();
+	input.value = name;
+})
